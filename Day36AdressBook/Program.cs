78ffa4771998caab277985fe60ac4f08d0e84bc1
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-10-19 21:15:59 +0530

    [roshan]Uc5 Search Person

--- Day36AdressBook/Program.cs
+++ Day36AdressBook/Program.cs
@@ -16,6 +16,7 @@ namespace Day36AdressBook
             Console.WriteLine("2. Add a contacts in address book");
             Console.WriteLine("3. Delete the contact in address book");
             Console.WriteLine("4. Add multiple person in address book");
+            Console.WriteLine("5. Search person in address book");
             Console.WriteLine("Enter a number");
             int option = Convert.ToInt32(Console.ReadLine());
 
@@ -34,6 +35,10 @@ namespace Day36AdressBook
                     break;
                 case 4:
                     AddMultiplePerson.AddMultiplePersonInAddressBook(list);
+                    break;
+                case 5:
+                    SearchPerson.SearchPersonInAddressBook(list);
+                    Console.WriteLine();
                     break;
                 default:
                     Console.WriteLine("Choose a appropriate option");
---
+++ Day36AdressBook/SearchPerson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day36AdressBook
+{
+    public class SearchPerson
+    {
+        public static void SearchPersonInAddressBook(List<Contacts> list)
+        {
+
+            foreach (var contact in list.FindAll(e => (e.City == "nagpur" && e.Address == "Nashik")).ToList())
+            {
+                contact.Show();
+
+                Console.WriteLine(contact.FirstName);
+            }
+        }
+    }
+}
